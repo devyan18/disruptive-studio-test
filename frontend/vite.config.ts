d7834786d125574d46e9cbc59e888b638ec703aa
devyan18 <sebba30015boga@gmail.com>
2024-05-02 19:20:30 -0300

    solved error dotenv in dotenv

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,7 @@
 /// <reference types="vite/client" />
 
-import "dotenv/config";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-
-console.log(process.env.VITE_SERVER_HOST);
 
 // https://vitejs.dev/config/
 export default defineConfig({
